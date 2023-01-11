331d95d6c16e2bd2e86c9117955b793c1a1ffd37
Miguel <miguezaga2@hotmail.com>
2023-01-11 13:47:36 -0500

    style mobile menu closed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// client-audiophile.svg
-// client-databiz.svg
-// client-maker.svg
-// client-meet.svg
+import clientAudiophile from "./images/client-audiophile.svg"
+import clientDatabiz from "./images/client-databiz.svg"
+import clientMaker from "./images/client-maker.svg"
+import clientMeet from "./images/client-meet.svg"
 // favicon-32x32.png
 // icon-arrow-down.svg
 // icon-arrow-up.svg
@@ -11,13 +11,20 @@ import iconMenu from "./images/icon-menu.svg"
 // icon-planning.svg
 // icon-reminders.svg
 // icon-todo.svg
-// image-hero-desktop.png
-// image-hero-mobile.png
+import imageHeroDesktop from "./images/image-hero-desktop.png"
+import imageHeroMobile from "./images/image-hero-mobile.png"
 import logo from "./images/logo.svg"
 
 import { useState } from "react";
 
 function App() {
+  var clientImages = [
+    clientDatabiz,
+    clientAudiophile,
+    clientMeet,
+    clientMaker,
+  ]
+
   return (
     <div>
       <header className="header">
@@ -60,17 +67,32 @@ function App() {
         </button> 
           */}
       </header>
-      <main>
-        <h1>
-          Make remote work
-        </h1>
-        <p>
-          Get your team in sync, no matter your location. Streamline processes,
-          create team rituals, and watch productivity soar.
-        </p>
-        <button>
-          Learn more
-        </button>
+      <main className="main">
+        <picture className="main__img">
+          <source srcSet={imageHeroDesktop} media="(min-width: 1024px)" />
+          <img src={imageHeroMobile} alt="person standing up holding a laptop" />
+        </picture>
+        <div className="main__container">
+          <h1 className="main__title">
+            Make remote work
+          </h1>
+          <p className="main__text">
+            Get your team in sync, no matter your location. Streamline processes,
+            create team rituals, and watch productivity soar.
+          </p>
+          <button className="main__btn">
+            Learn more
+          </button>
+          <ul className="main__clients">
+            {
+              clientImages.map((e, i) => (
+                <li key={i}>
+                  <img alt={`client logo number ${i}`} src={e} />
+                </li>
+              ))
+            }
+          </ul>
+        </div>
       </main>
     </div>
   );
