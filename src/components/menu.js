fd4ae36e97d26621889caca617c8b37f281e2e35
Miguel <miguezaga2@hotmail.com>
2023-01-13 16:50:50 -0500

    style close button menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,39 +2,43 @@ import iconCloseMenu from "../images/icon-close-menu.svg"
 
 export default ({ setOpen }) => (
   <aside className="menu">
-    <button onClick={() => setOpen(false)} className="menu__close">
-      <img src={iconCloseMenu} alt="close menu" />
-    </button>
-    <nav className="menu__nav">
-      <ul className="menu__list">
-        <li className="menu__li">
-          <button className="menu__accordion">
-            Features
-          </button>
-        </li>
-        <li className="menu__li">
-          <button className="menu__accordion">
-            Company
-          </button>
-        </li>
-        <li className="menu__li">
-          <a className="menu__a">
-            Careers
-          </a>
-        </li>
-        <li className="menu__li">
-          <a className="menu__a">
-            About
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="menu__container">
+      <button onClick={() => setOpen(false)} className="menu__close">
+        <img src={iconCloseMenu} alt="close menu" />
+      </button>
+      <nav className="menu__nav">
+        <ul className="menu__list">
+          <li className="menu__li">
+            <button className="menu__accordion">
+              Features
+            </button>
+          </li>
+          <li className="menu__li">
+            <button className="menu__accordion">
+              Company
+            </button>
+          </li>
+          <li className="menu__li">
+            <a className="menu__a">
+              Careers
+            </a>
+          </li>
+          <li className="menu__li">
+            <a className="menu__a">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-    <button className="menu__login">
-      Login
-    </button>
-    <button className="menu__register">
-      Register
-    </button>
+      <div className="menu__buttons">
+        <button className="menu__login">
+          Login
+        </button>
+        <button className="menu__register">
+          Register
+        </button>
+      </div>
+    </div>
   </aside>
 )
